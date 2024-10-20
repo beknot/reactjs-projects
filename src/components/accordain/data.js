59eea11c43d8fd66e8244ef3a1d872ed4a0f6313
binod.chakradhar <binod.chakradhar@ebpearls.com>
2024-10-20 20:24:25 +0545

    Accordian single-select, multi-select accordian added

---
+++ src/components/accordain/data.js
@@ -0,0 +1,29 @@
+const data = [
+    {
+        id : '1',
+        question : "What is name of our galaxy ?",
+        answer : "Milky Way Galaxy",
+    },
+    {
+        id : '2',
+        question : "How many planets are there ?",
+        answer : "Eight Planets",
+    },
+    {
+        id : '3',
+        question : "What is name of our planet ?",
+        answer : "Earth",
+    },
+    {
+        id : '4',
+        question : "What is nearest planet to us ?",
+        answer : "Venus",
+    },
+    {
+        id : '5',
+        question : "What is farthest planet to us ?",
+        answer : "Uranus",
+    }
+];
+
+export default data;
